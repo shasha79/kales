107e0b25b690f7370b8eb6184eb0be165c23496e
Alexander Raginsky <alexander.raginsky@gmail.com>
2018-01-08 17:59:46 +0700

    Update client accordint to latest OpenCalais API

--- kales.py
+++ kales.py
@@ -8,29 +8,21 @@ class Kales(object):
     def __init__(self, api_key):
         self.api_key = api_key
 
-    def _request(self, content, content_type, external_id, **kwargs):
+    def _request(self, content, content_type, **kwargs):
         headers = {
-            "x-calais-licenseID": self.api_key,
+            "x-ag-access-token": self.api_key,
             "content-type": content_type,
-            "externalID": external_id,
-            "accept": "application/json",
-            "enableMetadataType": "SocialTags",
-            "calculateRelevanceScore": "true",
-            "allowDistribution": "false",
-            "allowSearch": "false"
+            "outputFormat": "application/json",
         }
         headers.update(kwargs)
-        return requests.post("http://api.opencalais.com/tag/rs/enrich",
+        return requests.post("https://api.thomsonreuters.com/permid/calais",
             data=content, headers=headers)
 
     def analyze(self, content, content_type="text/raw", external_id=None, **kwargs):
         if not content or not content.strip():
             return None
 
-        if not external_id:
-            external_id = uuid.uuid4().hex
-
-        response = self._request(content, content_type, external_id, **kwargs)
+        response = self._request(content, content_type, **kwargs)
         response.raise_for_status()
         content = json.loads(response.content)
         for element in list(content.values()):
